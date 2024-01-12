c74935d67600226f28eb0244ff16b819e664932d
Purna <purnachbehara@gmail.com>
2024-01-12 12:46:23 +0530

    All project files uploaded to local git repository

---
+++ src/test/java/com/assignment/votecount/VotingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.votecount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
